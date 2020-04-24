7a904d6898d2f7ff9420d4179c4a3d4f2eba36d0
JleenDeniel <d.zhilin2013@yandex.ru>
2020-04-24 22:44:21 +0300

    В работе 12 добавлена функциональность по поиску самого повторяющегося слова в файле, по рассчету статистики по всем словам в файле и поиск определенного слова

--- FileAnalyzer/FileAnalyzer.cs
+++ FileAnalyzer/FileAnalyzer.cs
@@ -1,34 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.Json;
+using  System.Linq;
+
 
 namespace FileAnalyzer {
     public static class FileAnalyzer {
-        public static void Analyze(string input) {
-            var dict = CountWords(Replacer(input));
-            var jsonString = JsonSerializer.Serialize(dict);
-            
-            Console.WriteLine(jsonString);
+        public static Dictionary<string, int> Analyze(string input) {
+            var dict = new Dictionary<string, int>(CountWords(Replacer(input)));
+            return dict;
         }
 
-        private static Dictionary<string, int> CountWords(string input) {
+        
+        public static Dictionary<string, int> MostFrequentWord(Dictionary<string, int> dictionary){
+            var resultDict = new Dictionary<string, int>();
+            var maxValue = dictionary.Values.Max();
+            foreach (var pair in dictionary) {
+                if (pair.Value == maxValue) {
+                    resultDict[pair.Key] = maxValue;
+                }
+            }
+
+            return resultDict;
+        }
+
+        public static Dictionary<string, int> MostFrequentWord(string input){
+            var res = Analyze(Replacer(input));
+            return MostFrequentWord(res);
+        }
+
+            private static Dictionary<string, int> CountWords(string input) {
             Dictionary<string, int> dict = new Dictionary<string, int>();
-            string[] wordsArray = input.Split(" ");
+            string[] wordsArray = input.Split(new char[]{' '}, StringSplitOptions.RemoveEmptyEntries);
             foreach (var word in wordsArray) {
-                if (!dict.ContainsKey(word)) {
-                    dict.Add(word, 1);
+                if (!dict.ContainsKey(word.ToLower())) {
+                    dict.Add(word.ToLower(), 1);
                 }
                 else {
-                    dict[word]++;
+                    dict[word.ToLower()]++;
                 }
 
             }
             return dict;
         }
 
+        public static string[] FindWord(string text, string wordToFind){ 
+            var dict = Analyze(text); 
+            if (!dict.ContainsKey(wordToFind)) { 
+                throw new ArgumentException("No such word");
+            } 
+            string[] res = new string[1]; 
+            res[0] = wordToFind.Length.ToString();
+            return res;
+        }
+
         private static string Replacer(string input) {
-            input.Replace(",", string.Empty).Replace(".", string.Empty).Replace("/t", string.Empty);
-            return input;
+            string res = input.Replace(",", string.Empty).Replace(".", string.Empty).Replace("/t", string.Empty)
+                .Replace("!",string.Empty).Replace("?", string.Empty);
+            return res;
         }
 
         
--- FileAnalyzer/Program.cs
+++ FileAnalyzer/Program.cs
@@ -1,14 +1,38 @@
 ﻿using System;
+using System.IO;
 using System.Text;
-using System.Threading;
 
 
 namespace FileAnalyzer {
     class Program {
         public static void Main(string[] args) {
-            FileAnalyzer.Analyze("Hi, I am Dany, I am from Russia and I am glad to be a part of this team");
-            
-             
+            var str = new StringBuilder();
+            try {
+                using var sr = new StreamReader(@"D:\WorkFolder\C#\CsharpUniversityWorks\CsharpUniversityWorks\FileAnalyzer\Big.txt");
+                while (!sr.EndOfStream) {
+                    string fileStr = sr.ReadLine();
+                    str.Append(fileStr);
+                }
+            }
+            catch (FileNotFoundException) {
+                Console.WriteLine("File not found!");
+            }
+
+            var dictAboutWholeFile = FileAnalyzer.Analyze(str.ToString());
+            Console.WriteLine("Info about all words from file:");
+            foreach (var pair in dictAboutWholeFile) {
+                Console.WriteLine($"{pair.Key} - {pair.Value}");
+            }
+
+            Console.WriteLine("Самые часто встречающиеся слова(слово):");
+            var dictAboutFrequency = FileAnalyzer.MostFrequentWord(str.ToString());
+            foreach (var pair in dictAboutFrequency) {
+                Console.WriteLine($"{pair.Key} - {pair.Value}");
+            }
+
+            Console.WriteLine("Введите слово, которое хотите найти:");
+            var info = FileAnalyzer.FindWord(str.ToString(), Console.ReadLine());
+            Console.WriteLine($"The length of this word is {info[0]}");
         }
     }
 
